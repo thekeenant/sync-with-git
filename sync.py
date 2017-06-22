03180dd0f03242fb506cc46aa46e22ea62334b03
thekeenant <keenan@keenant.com>
2017-06-22 16:38:39 -0500

    Initial commit

---
+++ sync.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+
+import yaml
+import sys
+import os
+from subprocess import call
+from distutils.dir_util import copy_tree, remove_tree
+
+config = "sync.yml"
+tmp = "sync"
+
+if len(sys.argv) > 1:
+  config = sys.argv[1]
+
+if len(sys.argv) > 2:
+  tmp = sys.argv[2]
+
+if len(sys.argv) > 3:
+  print("Invalid usage: ./sync.py <config filename> <tmp directory>")
+
+print("Using configuration: " + config)
+print("Using tmp directory: " + tmp)
+
+# rm -rf tmp directory
+if os.path.exists(tmp):
+  print("Cleaining existing tmp...")
+  remove_tree(tmp)
+
+with open(config, "r") as file:
+  data = yaml.load(file)
+  git = data["git"]
+  abort = data["abort-on-failure"]
+  repos = data["repositories"]
+
+  successes = 0
+  total = len(repos)
+
+  for repo in repos:
+    url = repo["url"]
+    branch = repo["branch"]
+    target = repo["target"]
+
+    print("=== {} ({}) --> {} ===".format(url, branch, target))
+
+    # git clone repo into tmp directory
+    print("Cloning repository...")
+    call(" ".join([
+      git,
+      "clone",
+      "--depth=1",
+      "-b " + branch,
+      url,
+      tmp
+    ]), shell=True)
+
+    dotgit = os.path.join(tmp, ".git")
+
+    if not os.path.exists(dotgit):
+      print("WARNING: Nothing cloned/found!")
+
+      if abort:
+        print("Aborting sync, as defined in configuration.")
+        break
+      else:
+        continue
+    else:
+      successes += 1
+
+    # unnecessary junk
+    print("Removing .git...")
+    remove_tree(dotgit)
+
+    # move to desired folder
+    print("Moving to target...")
+    copy_tree(tmp, target)
+
+    # rm -rf tmp directory
+    print("Cleaning...")
+    remove_tree(tmp)
+
+print("====================")
+print("Sync completed ({}/{})".format(successes, total))
